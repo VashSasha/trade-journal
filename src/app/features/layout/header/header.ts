59353e97a8745c520a6a6aa260e09fc499b3bcd3
ovashchuk <oleksandr@elegenbio.com>
2026-07-19 00:43:13 -0500

    adding - save ai analyses response

--- src/app/features/layout/header/header.ts
+++ src/app/features/layout/header/header.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ThemeService } from '../../../core/services/theme.service';
 import { AccountSelectorComponent } from './account-selector/account-selector.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AccountSelectorComponent],
+  imports: [AccountSelectorComponent, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
